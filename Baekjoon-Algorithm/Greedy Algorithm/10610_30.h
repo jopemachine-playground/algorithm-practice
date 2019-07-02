b9a3d312a3cdd3fce653b36c5ddc8d080ef345a8
jopemachine <jopemachine@naver.com>
2019-07-02 13:36:54 +0900

    update

---
+++ Baekjoon-Algorithm/Greedy Algorithm/10610_30.h
@@ -0,0 +1,21 @@
+//
+// Created by wonma on 2019-07-02.
+//
+
+#ifndef SELFMADE_ALGORITHM_10610_30_H
+#define SELFMADE_ALGORITHM_10610_30_H
+
+#include <iostream>
+
+using namespace std;
+
+void solve_10610(){
+    // N은 최대 100000 개의 숫자로 구성된 양수
+    long long int N;
+    cin >> N;
+
+
+
+}
+
+#endif //SELFMADE_ALGORITHM_10610_30_H
