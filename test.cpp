a3145512cd199b0233e4679be37114bb874807b6
jopebot <jopemachine@naver.com>
2019-07-23 16:12:11 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 // #include "Baekjoon-Algorithm/Dijkstra/1916.h"
-#include "Algorithm-DS-Practice/Data-Structure/Sort/RadixSort.h"
+#include "Algorithm-DS-Practice/Data-Structure/Sort/BucketSort.h"
 
 int main() {
     // solve_1916();
-    test_radixSort();
+    test_bucketSort();
 }
