630e352a6df41681eddfb75c8283ecef92c27ef3
jopemachine <jopemachine@naver.com>
2019-07-29 21:17:44 +0900

    no comment

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/SelfMade/Algorithm")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Algorithm")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
