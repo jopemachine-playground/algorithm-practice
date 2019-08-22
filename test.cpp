4d9e15c43747a0ad6a5ed7d40d7cf1e2d780dab8
jopemachine <jopemachine@naver.com>
2019-08-22 16:13:13 +0900

    Add 1930.h

--- test.cpp
+++ test.cpp
@@ -2,8 +2,8 @@
 
 // #include "Baekjoon-Algorithm/Dijkstra/1753.h"
 // #include "Algorithm-DS-Practice/Data-Structure/List/ArrayList.h"
-#include "Baekjoon-Algorithm/Divide and Conquer/2630.h"
+#include "Baekjoon-Algorithm/Divide and Conquer/1930.h"
 int main() {
     // solve_1753();
-    solve_2630();
+    solve_1930();
 }
