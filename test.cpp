7fa816f1292c4bb100378e8fcf43a049e3f6b34a
jopebot <jopemachine@naver.com>
2019-09-08 17:07:20 +0900

    Add 1697

--- test.cpp
+++ test.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 
-#include "Baekjoon-Algorithm/Dijkstra/1753.h"
+#include "Baekjoon-Algorithm/BFS/1697.h"
 // #include "Algorithm-DS-Practice/Data-Structure/List/ArrayList.h"
 // #include "Baekjoon-Algorithm/DFS/11724.h"
 
 int main() {
-    solve_1753();
-
+    solve_1697();
 }
