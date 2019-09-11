7ab8b8fba8c20a79dee5979d429315a46e1e58a2
jopebot <jopemachine@naver.com>
2019-09-12 08:42:10 +0900

    Add 2606

--- test.cpp
+++ test.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-#include "Baekjoon-Algorithm/Math/1978.h"
+#include "Baekjoon-Algorithm/BFS/2606.h"
 // #include "Algorithm-DS-Practice/Data-Structure/List/ArrayList.h"
 // #include "Baekjoon-Algorithm/DFS/11724.h"
 
 int main() {
-    solve_1978();
+    solve_2606();
 }
