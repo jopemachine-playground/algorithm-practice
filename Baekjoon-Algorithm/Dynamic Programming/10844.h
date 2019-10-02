1908f504aa4460af40f12e78e4b8363629fbe6f2
jopebot <jopemachine@naver.com>
2019-10-02 09:05:38 +0900

    Add Some DP problems

--- Baekjoon-Algorithm/Dynamic Programming/10844.h
+++ Baekjoon-Algorithm/Dynamic Programming/10844.h
@@ -1,22 +1,48 @@
-// https://js1jj2sk3.tistory.com/38
+/*
+==============================+===============================================================
+@ File Name : 10844.h
+@ Author : jopemachine
+@ Desc : 
+@    쉬운 계단수
+@    점화식을 도저히 찾을 수가 없었다. 점화식을 만들 때, n번째 길이의 계단 수 갯수를 
+@    뒤에 어떤 숫자가 오는지에 따라 케이스를 나누기 위해, 이차원 배열로 만들면 점화식이 구해진다.
+@ Ref URLs : 
+@    https://js1jj2sk3.tistory.com/38
+==============================+===============================================================
+*/
 #include <iostream>
+#include <algorithm>
+#include <numeric>
 
 using namespace std;
 
-int f[101];
+long dp[101][10];
+int mod = 1000000000;
 
 void solve(){
     int N;
     cin >> N;
 
-    f[0] = 0;
-    f[1] = 9;
-    f[2] = 17;
-
-    for(int i = 2; i <= N; i++){
-        f[i] = (2 * f[i-1] - (i - 1)) % 1000000000;
+    // base case
+    dp[1][0] = 0;
+    for(int i = 1; i <= 9; i++){
+        dp[1][i] = 1;
     }
 
-    cout << f[N];
+    for(int i = 2; i <= N; i++){
+        for(int j = 0; j <= 9; j++){
+            if      (j == 0) dp[i][j] = (dp[i-1][j+1]) % mod;
+            else if (j == 9) dp[i][j] = (dp[i-1][j-1]) % mod;
+            else             dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % mod;
+        }
+    }
 
+    long sum = 0;
+//    cout << accumulate(dp[N], dp[N] + 10, 0);
+
+    for (int elem: dp[N]){
+        sum = (sum + elem) % mod;
+    }
+
+    cout << sum;
 }
--- t.cpp
+++ t.cpp
@@ -1,4 +1,4 @@
-#include "Baekjoon-Algorithm/Dynamic Programming/9461.h"
+#include "Baekjoon-Algorithm/Dynamic Programming/1904.h"
 // #include "Algorithm-DS-Practice/Data:-Structure/List/ArrayList.h"
 // #include "Baekjoon-Algorithm/DFS/11724.h"
 
