36c63ab3839ab1974f8c0012df29fdc415b8cb98
jopemachine <gravityc95@gmail.com>
2020-10-28 13:49:21 +0900

    Update 42746.cpp

--- Programmers/Sort/42746.cpp
+++ Programmers/Sort/42746.cpp
@@ -5,54 +5,57 @@
 
 using namespace std;
 
-#define DEBUG
+//#define DEBUG
 
 class NumInfo {
 public:
     int index;
     int number;
-    int original;
 };
 
 string solution(vector<int> numbers) {
     string answer = "";
 
     // index, number value
-    vector<NumInfo> numIdxs;
+    vector<NumInfo> numInfos;
     int idx = 0;
-    for (auto number : numbers) {
-        int original = number;
-        int lastNum = number % 10;
 
+    for (auto number : numbers) {
         if (number < 10) {
-            number = number * 1000 + lastNum * 111;
+            number = number * 1000 + number * 111;
         }
         else if (10 <= number && number < 100) {
-            number = number * 100 + lastNum * 11;
+            number = number * 100 + number;
         }
         else if (100 <= number && number < 1000) {
-            number = number * 10 + lastNum;
+            number = number * 10 + (number / 100);
         }
 
-        numIdxs.push_back(NumInfo { idx, number, original });
+        numInfos.push_back(NumInfo{ idx++, number });
     }
-
-    sort(numIdxs.begin(), numIdxs.end(), [] (NumInfo ls, NumInfo rs) -> bool {
-        if (ls.number == rs.number) {
-            return ls.original < rs.original;
-        }
+    
+    sort(numInfos.begin(), numInfos.end(), [] (NumInfo ls, NumInfo rs) -> bool {
         return ls.number > rs.number;
     });
 
     #ifdef DEBUG
-    for (auto numIdx : numIdxs) {
-        cout << numIdx.second << endl;
+    for (auto numInfo : numInfos) {
+        cout << numInfo.number << endl;
     }
     #endif
 
-    for (auto numIdx : numIdxs) {
-        answer += to_string(numbers[numIdx.first]);
+    bool firstZero = true;
+    for (auto numInfo : numInfos) {
+        if (numbers[numInfo.index] != 0) {
+            firstZero = false;
+        }
+        if (!firstZero) {
+            answer += to_string(numbers[numInfo.index]);
+        }
     }
+    
+    if (numbers.size() == 0) return "";
+    if (firstZero) return "0";
 
     return answer;
 }
