2dcb96fe5d72b7b3dbdd1b892c51077f117052eb
jopemachine <gravityc95@gmail.com>
2020-11-15 12:21:03 +0900

    Create 60057.cpp

---
+++ Programmers/Level2/60057.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = s.length();
+
+    // length should be greater than 0 and less than (len / 2) + 1
+    for (int len = 0; len <= s.length() / 2; ++len) {
+        string compressedStr = "";
+        string repeatedStr = s.substr(0, len);
+        int cnt = 1;
+
+        for (int j = len; j < s.length(); j += len) {
+            if (repeatedStr == s.substr(j, len)) {
+                ++cnt;
+            }
+            else {
+                if (cnt == 1) {
+                    compressedStr += repeatedStr;
+                }
+                else {
+                    compressedStr += (to_string(cnt) + repeatedStr);
+                }
+
+                repeatedStr = s.substr(j, len);
+                cnt = 1;
+            }
+        }
+
+        if ((s.length() % len) != 0) {
+            compressedStr += s.substr((s.length() / len) * len);
+        }
+
+        if (answer > compressedStr.length()) answer = compressedStr.length();
+    }
+    
+    return answer;
+}
