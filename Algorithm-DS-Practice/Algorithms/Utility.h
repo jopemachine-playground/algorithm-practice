ed3723aa4706dbd6dd6f066b5d7097d71fdc0286
Gyubong Lee <jopemachine@naver.com>
2022-01-14 15:44:30 +0900

    move

---
+++ Algorithm-DS-Practice/Algorithms/Utility.h
@@ -0,0 +1,35 @@
+#pragma once
+template<class T>
+void swap(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+int ascending_compare(void* a, void* b)
+{
+	int num1 = *(int*)a;
+	int num2 = *(int*)b;
+
+	if (num1 > num2)
+		return -1;
+
+	if (num1 < num2)
+		return 1;
+
+	return 0;
+}
+
+int descending_compare(void* a, void* b)
+{
+	int num1 = *(int*)a;
+	int num2 = *(int*)b;
+
+	if (num1 > num2)
+		return 1;
+
+	if (num1 < num2)
+		return -1;
+
+	return 0;
+}
