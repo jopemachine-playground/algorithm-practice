b5117608aff5e0cabaec90c7839f1da7c36d4dc1
Gyubong Lee <jopemachine@naver.com>
2022-02-14 13:12:30 +0900

    15651 15652

---
+++ Baekjoon-Algorithm/Backtracking/15651.js
@@ -0,0 +1,36 @@
+const readline = require('readline');
+
+const inputs = [];
+
+readline
+  .createInterface(process.stdin, {})
+  .on('line', function(line) {
+    inputs.push(line.trim());
+  }).on('close', function() {
+    solve();
+  });
+
+const solve = () => {
+  const [N, M] = inputs[0].split(' ').map(e => +e);
+
+  let ans = [];
+  
+  const pick = (picked = [], idx = 0) => {
+    let result = [];
+    if (idx === M) {
+      ans.push([...picked]);
+      return;
+    }
+  
+    for (let i = 1; i <= N; ++i) {
+      picked.push(i);
+      pick(picked, idx + 1);
+      picked.pop();
+    }
+  
+    return result;
+  };
+
+  pick();
+  console.log(ans.map(str => str.join(' ')).join('\n'));
+};
