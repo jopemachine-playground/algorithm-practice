6d0dc997c2b77eeb935592806ef389f30c0dfbc8
Gyubong Lee <jopemachine@naver.com>
2022-02-16 14:11:15 +0900

    Update 2749.cpp

--- Baekjoon-Algorithm/Math/2749.cpp
+++ Baekjoon-Algorithm/Math/2749.cpp
@@ -27,7 +27,6 @@ int solve() {
   // 다른 코드로 먼저 주기를 찾아둬도 되고 사실 그냥 위에 주기 찾는 코드를 넣어줘도 된다.
   // Ref: https://www.acmicpc.net/problem/9471
   int T = 1500000;
-  long long nums[T];
 
   long long prev = 0;
   long long next = 1;
