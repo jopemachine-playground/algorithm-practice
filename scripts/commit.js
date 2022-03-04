f7957f31f3b6d4dbf185873eb9a80d66c5c3aec7
Gyubong Lee <jopemachine@naver.com>
2022-03-04 10:59:31 +0900

    Upd

--- scripts/commit.js
+++ scripts/commit.js
@@ -2,11 +2,13 @@ import {execa, execaCommand} from 'execa';
 import logSymbols from 'log-symbols';
 import {globby} from 'globby';
 import path from 'node:path';
+import chalk from 'chalk';
+import inquirer from 'inquirer';
 
 const problemNumber = process.argv[2];
 
 (async () => {
-  const paths = await globby(`**/${problemNumber}*.*`);
+  let paths = await globby(`**/${problemNumber}*.*`);
 
   if (paths.length === 0) {
     console.error(`${logSymbols.error} Failed to find '${problemNumber}'`);
@@ -14,14 +16,27 @@ const problemNumber = process.argv[2];
   }
 
   if (paths.length > 1) {
-    console.error('Failed to set problem.');
-  } else {
-    const { dir } = path.parse(paths[0]);
-    await execa('git', ['add', paths[0]]);
-    // console.log(`git commit -m "\[${dir}\] Solve ${problemNumber}"`);
-    await execa('git', ['commit', '-m', `[${dir}] Solve ${problemNumber}`]);
-    await execaCommand(`git push origin master`);
-    console.log(`${logSymbols.success} Commit and push problem successfully: '${paths[0]}'`);
+    await inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'file',
+          message: chalk.dim('Choose file to commit'),
+          choices: paths.map((path) => {
+            return { name: path, value: path };
+          })
+        }
+      ])
+      .then(selection => {
+        paths = [selection.file];
+      });
   }
+
+  const { dir } = path.parse(paths[0]);
+  await execa('git', ['add', paths[0]]);
+  // console.log(`git commit -m "\[${dir}\] Solve ${problemNumber}"`);
+  await execa('git', ['commit', '-m', `[${dir}] Solve ${problemNumber}`]);
+  await execaCommand(`git push origin master`);
+  console.log(`${logSymbols.success} Commit and push problem successfully: '${paths[0]}'`);
 
 }) ();
--- scripts/setProblem.js
+++ scripts/setProblem.js
@@ -1,5 +1,7 @@
 import {globby} from 'globby';
 import logSymbols from 'log-symbols';
+import chalk from 'chalk';
+import inquirer from 'inquirer';
 import process from 'node:process';
 import fs from 'node:fs';
 const fsp = fs.promises;
@@ -9,7 +11,7 @@ import './clean.js';
 const problemNumber = process.argv[2];
 
 (async () => {
-  const paths = await globby(`**/${problemNumber}*.*`);
+  let paths = await globby(`**/${problemNumber}*.*`);
 
   if (paths.length === 0) {
     console.error(`${logSymbols.error} Failed to find '${problemNumber}'`);
@@ -17,15 +19,28 @@ const problemNumber = process.argv[2];
   }
 
   if (paths.length > 1) {
-    console.error('Failed to set problem.');
-  } else {
-    console.log(`${logSymbols.success} Set problem path successfully: '${paths[0]}'`);
-    const execFileContent = await fsp.readFile('t.cpp', { encoding: 'utf-8' });
-    const [_, ...content] = execFileContent.split('\n');
-
-    await fsp.writeFile('t.cpp', [
-      `#include "${paths[0]}"`,
-      ...content
-    ].join('\n'), { encoding: 'utf-8' });
+    await inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'file',
+          message: chalk.dim('Choose file to commit'),
+          choices: paths.map((path) => {
+            return { name: path, value: path };
+          })
+        }
+      ])
+      .then(selection => {
+        paths = [selection.file];
+      });
   }
+
+  console.log(`${logSymbols.success} Set problem path successfully: '${paths[0]}'`);
+  const execFileContent = await fsp.readFile('t.cpp', { encoding: 'utf-8' });
+  const [_, ...content] = execFileContent.split('\n');
+
+  await fsp.writeFile('t.cpp', [
+    `#include "${paths[0]}"`,
+    ...content
+  ].join('\n'), { encoding: 'utf-8' });
 })();
